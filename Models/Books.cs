886dea5863c34476e6a26a0c2ba8b61cd9d16ead
Andri Bonilla <guzmanbonilla7@gmail.com>
2022-02-25 17:39:37 -0400

    Agregar logo y vista de comprasAdd project files.

---
+++ Models/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_Space.Models
+{
+    public class Books
+    {
+        public int idlibro { get; set; }
+        public string nombre { get; set; }
+        public int autor { get; set; }
+        public string genero { get; set; }
+        public decimal precio { get; set; }
+        public string editorial { get; set; }
+        public int Cant_disponible { get; set; }
+    }
+}
